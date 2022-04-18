111dfe20024c6ee4f3f80d1208643aa3f1524898
Zauri Metreveli <72525534+Metrevelii@users.noreply.github.com>
2022-04-18 15:04:26 +0400

    40 children props

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,16 +1,18 @@
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 import "./ExpenseItem.css";
 
 // Adding new component -- expense item
 // We need parameter to adjust Dynamic data (data-attributes) to elements. for that we use - props object
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
