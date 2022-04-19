56d3bf535c0292fe7a12c4e42101c840407b2ade
Zauri Metreveli <72525534+Metrevelii@users.noreply.github.com>
2022-04-19 13:52:05 +0400

    usestate hook

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,31 @@
-import React from 'react';
+// import other pieces from react library
+import React, { useState } from 'react';
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 // Adding new component -- expense item
 // We need parameter to adjust Dynamic data (data-attributes) to elements. for that we use - props object
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  // hooks must be called inside directal functions
+  // useState returns array where first value is variable itself (value) and second is updating function
+  const [title, setTitle] = useState(props.title);
+  
+  
+  const clickHandler = () => {
+    // title = 'Updated!';
+    setTitle('Updated!');
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
